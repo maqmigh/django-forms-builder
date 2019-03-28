4427092899d7ade54da3f9470cd6410a599c0757
martin <m@pikka.org>
2019-03-28 09:45:44 +0100

    form embed view...

--- forms_builder/forms/urls.py
+++ forms_builder/forms/urls.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+
+from django.views.generic import TemplateView
+
 try:
     from django.urls import re_path
 except ImportError:
@@ -9,6 +12,7 @@ from forms_builder.forms import views
 
 
 urlpatterns = [
+    re_path(r"(?P<slug>.*)/embed/$", TemplateView.as_view(template_name='forms/form_embed.html'), name="form_embed"),
     re_path(r"(?P<slug>.*)/sent/$", views.form_sent, name="form_sent"),
     re_path(r"(?P<slug>.*)/$", views.form_detail, name="form_detail"),
 ]
