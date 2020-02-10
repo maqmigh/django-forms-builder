357f9fdfa73c40a1266501b0afbf956898bc3d15
martin <m@pikka.org>
2020-02-10 12:10:14 +0100

    install requires... < Django 4

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ try:
             "sphinx-me >= 0.1.2",
             "unidecode",
             "django-email-extras >= 0.2",
-            "django >= 1.8, <= 3",
+            "django >= 1.8, <= 4",
             "future <= 0.18.2",
         ],
         classifiers = [
